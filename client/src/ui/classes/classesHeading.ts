d7997484780e27fab2ae35ffd7e051a78960b090
David Siewert <david1gruppenplan@gmail.com>
2026-08-13 17:59:03 +0200

    style(ui): remove serif font classes

--- client/src/ui/classes/classesHeading.ts
+++ client/src/ui/classes/classesHeading.ts
@@ -1,6 +1,6 @@
 import { classArr } from "../classArr"
 
 export const classesHeading = classArr(
-  "mt-[7px] mb-0 text-foreground font-serif text-[clamp(29px,7vw,38px)] font-medium",
+  "mt-[7px] mb-0 text-foreground text-[clamp(29px,7vw,38px)] font-medium",
   "leading-[1.08] tracking-[-0.025em] focus:outline-none",
 )
